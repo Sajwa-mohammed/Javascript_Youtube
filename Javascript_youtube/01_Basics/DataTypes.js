8487c2725e200cfdfed77d260f99b1a8aeaef43f
Sajwa-mohammed <141017572+Sajwa-mohammed@users.noreply.github.com>
2026-08-18 15:50:10 +0000

    varaibales

---
+++ Javascript_youtube/01_Basics/DataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+
+// alert("hello")   // we are using nodejs, not browser
+// alert(3+3)
+
+console.log(3
+    +
+    3)  // code readability should be high 
+console.log("Hii")
+
+let name ="sajwa"
+let age=23
+let isLoggedIn=false
+
+
+
+//number => 2to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
+
+
---
+++ Javascript_youtube/01_Basics/variables.js
@@ -0,0 +1,17 @@
+const accountId=123455
+let accountName="Sajwa"
+var accountPassword="5555"
+accountCity="Hyd"
+let accountEmail;
+
+// accountId="2222"  // not allowed 
+
+accountName="Sana"
+accountPassword="654"
+accountCity="AP"
+
+//avoid using var because of issue  in block scope and functional scope
+
+console.log(accountId)
+console.table([accountName,accountPassword,accountCity,accountEmail])
+
